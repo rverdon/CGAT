021925ff17a21904d1d7815c9d1182ba3bdcb4d1
Eriq Augustine <eaugusti@chromium.org>
2012-11-14 23:27:22 -0800

    view-annotation is stubbed out.

--- ui/js/view-annotation.js
+++ ui/js/view-annotation.js
@@ -1,27 +1,27 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function() {
-   enableLoadingModal('contig');
+   enableLoadingModal('view-annotation');
 
    if (!window.params.id) {
-      enableErrorModal("No Contig Specified", 'contig');
+      enableErrorModal("No Annotation Specified", 'view-annotation');
       return;
    }
 
    $.ajax({
-      url: 'api/contig',
+      url: 'api/annotation',
       dataType: 'json',
       data: {id: window.params.id},
       error: function(jqXHR, textStatus, errorThrown) {
-         enableErrorModal('Fetching Contig', 'contig');
+         enableErrorModal('Fetching Annotation', 'view-annotation');
       },
       success: function(data, textStatus, jqXHR) {
          if (!data.valid) {
-            enableErrorModal('Invalid Contig', 'contig');
+            enableErrorModal('Invalid Annotation', 'view-annotation');
             return;
          }
 
-         setSubtitle(data.info.contig.meta.name);
+         setSubtitle(data.annotation.isoform_name);
 
          //TODO: Place the info.
          console.log(data);
--- ui/view-annotation.php
+++ ui/view-annotation.php
@@ -1,11 +1,42 @@
 <?php
    require_once('header.php');
-   makeHeader('CGAT', 'View Annotation');
+   makeHeader('CGAT Annotation', '',
+              array(),
+              array('view-annotation.js'));
 ?>
 
 <div id='content' class='top-level-area'>
-   <h1>View Annotation</h1>
-   <p>This page will be for viewing a specific annotation</p>
+
+   <div id='annotation-info' class='second-level-area'>
+      <h2 onclick='toggleCollapse("annotation-info-collapse-button", "annotation-info-area");'>
+         <div class='collapse-button' id='annotation-info-collapse-button'></div>
+         Annotation Info
+      </h2>
+      <div id='annotation-info-area' class='collapsing-area'>
+         TODO
+      </div>
+   </div>
+
+   <div id='exons' class='second-level-area'>
+      <h2 onclick='toggleCollapse("exons-collapse-button", "exons-area");'>
+         <div class='collapse-button collapse-on' id='exons-collapse-button'></div>
+         Exons
+      </h2>
+      <div id='exons-area' class='collapsing-area collapse'>
+         TODO
+      </div>
+   </div>
+
+   <div id='contig-info' class='second-level-area'>
+      <h2 onclick='toggleCollapse("contig-info-collapse-button", "contig-info-area");'>
+         <div class='collapse-button collapse-on' id='contig-info-collapse-button'></div>
+         Contig Info
+      </h2>
+      <div id='contig-info-area' class='collapsing-area collapse'>
+         TODO
+      </div>
+   </div>
+
 </div>
 
 <?php
