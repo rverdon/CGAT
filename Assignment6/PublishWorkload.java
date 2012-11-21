aeb3b85a5644eca70c1e892ba1685a71a0d39290
Aldrin Montana <DrinMontana@gmail.com>
2012-11-20 16:52:40 -0800

    Committing PublishWorkload so I can turn my computer off because eriq sucks. Also, it doesn't work.

--- Assignment6/PublishWorkload.java
+++ Assignment6/PublishWorkload.java
@@ -1,5 +1,8 @@
-import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.Map;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
 
 import java.sql.Connection;
@@ -10,14 +13,22 @@ import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.sql.Date;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.couchbase.client.CouchbaseClient;
+import net.spy.memcached.internal.GetFuture;
+import net.spy.memcached.internal.OperationFuture;
+
 /**
  * This one measures fetching a complete user's profile.
  */
 public class PublishWorkload extends Workload {
-   private static final int TIMES = 1000000;
-   //private static final int TIMES = 10;
+   //private static final int TIMES = 1000000;
+   private static final int TIMES = 10;
 
    private String[] userIds, annotationIds, exp;
+   List<String> annotationIdList, userIdList;
 
    /**
     * Fetch the user ids here, and preserve the connection.
@@ -50,6 +61,7 @@ public class PublishWorkload extends Workload {
    protected void cleanupMySQL() {
       super.cleanupMySQL();
       userIds = null;
+      annotationIds = null;
    }
 
    protected Stats executeMySQLImpl() {
@@ -87,10 +99,43 @@ public class PublishWorkload extends Workload {
    }
 
    protected void initCouch() {
+      super.initCouch();
+
+      Random rand = new Random(4);
+
+      annotationIdList = new ArrayList<String>(TIMES);
+      userIdList = new ArrayList<String>(TIMES);
+
+      for (int repeat = 0; repeat < TIMES; repeat++) {
+         annotationIdList.add(String.format("Annotations-%d", rand.nextInt(1000)));
+         userIdList.add(String.format("Users-%d", rand.nextInt(100)));
+      }
    }
 
    protected Stats executeCouchImpl() {
-      // Nope
-      throw new UnsupportedOperationException();
+      JSONObject userJSON = null, annotationJSON = null, contigJSON = null;
+
+      for (int iter_ndx = 0; iter_ndx < TIMES; iter_ndx++) {
+         Object user = client.get(userIdList.get(iter_ndx));
+         Object annotation = client.get(annotationIdList.get(iter_ndx));
+
+         try {
+            userJSON = new JSONObject(user.toString());
+            annotationJSON = new JSONObject(annotation.toString());
+
+            int contigId = annotationJSON.getInt("contig_id");
+            Object contig = client.get("Contigs-" + String.valueOf(contigId));
+            contigJSON = new JSONObject(contig.toString());
+         }
+         catch (Exception ex) {
+            System.err.println("Error in PublishWorkload Couchbase code: " + ex);
+            System.exit(1);
+         }
+
+         //TODO: Still need to increase the experience gained in user meta
+         //table as well as add annotation to user history?
+      }
+
+      return new Stats();
    }
 }
