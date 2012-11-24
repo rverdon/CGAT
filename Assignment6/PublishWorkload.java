caf5871a4420e09750e2a6c8031361edd8cc010a
rhnarakis <ec2-user@ip-10-168-85-10.us-west-1.compute.internal>
2012-11-24 02:02:09 +0000

    Committing changes to PublishWorkload. This should be close to functional though it still needs testing.

--- Assignment6/PublishWorkload.java
+++ Assignment6/PublishWorkload.java
@@ -13,6 +13,7 @@ import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.sql.Date;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -100,7 +101,6 @@ public class PublishWorkload extends Workload {
 
    protected void initCouch() {
       super.initCouch();
-      /*
 
       Random rand = new Random(4);
 
@@ -111,36 +111,134 @@ public class PublishWorkload extends Workload {
          annotationIdList.add(String.format("Annotations-%d", rand.nextInt(1000)));
          userIdList.add(String.format("Users-%d", rand.nextInt(100)));
       }
-      */
    }
 
    protected Stats executeCouchImpl() {
-      /*
       JSONObject userJSON = null, annotationJSON = null, contigJSON = null;
 
       for (int iter_ndx = 0; iter_ndx < TIMES; iter_ndx++) {
-         Object user = client.get(userIdList.get(iter_ndx));
-         Object annotation = client.get(annotationIdList.get(iter_ndx));
+         String user_id = userIdList.get(iter_ndx);
+         String anno_id = annotationIdList.get(iter_ndx);
+
+         /*
+          * Retrieve the user and annotation documents. Everything revolves
+          * around these (to some extent)
+          */
+         Object user = client.get(user_id);
+         Object annotation = client.get(anno_id);
 
          try {
             userJSON = new JSONObject(user.toString());
             annotationJSON = new JSONObject(annotation.toString());
 
-            int contigId = annotationJSON.getInt("contig_id");
-            Object contig = client.get("Contigs-" + String.valueOf(contigId));
+            /*###############################################################*/
+            //CONTIG DIFFICULTY
+
+            /*
+             * Get the contig document based on the contig_id in the annotation
+             * document
+             */
+            String contigId = annotationJSON.getString("contig_id");
+            Object contig = client.get("Contigs-" + contigId);
             contigJSON = new JSONObject(contig.toString());
+
+            //Get the difficulty from the contig document
+            Integer contigDifficulty = Integer.parseInt(contigJSON.getString("difficulty"));
+
+            /*###############################################################*/
+            //USER META
+
+            /*
+             * Get the meta object from the user document. This contains the
+             * user's total experience
+             */
+            JSONObject userMeta = new JSONObject(userJSON.getJSONObject("meta"));
+            Integer userXP = Integer.parseInt(userMeta.getString("exp"));
+
+            /*
+             * The user's total experience is the user's previous experience
+             * plus the difficulty of the contig
+             */
+            int xpTotal = contigDifficulty.intValue() + userXP.intValue();
+
+            //replace the user's experience value in meta with the user's new
+            //experience total
+            userMeta.put("exp", String.valueOf(xpTotal));
+
+            //replace the user's meta object
+            userJSON.put("meta", userMeta);
+
+            /*###############################################################*/
+            //USER HISTORY
+
+            /*
+             * Add annotation to user history
+             */
+            JSONArray userHistory = userJSON.getJSONArray("history");
+
+            //build a meta object to add to the user's history array
+            JSONObject historyMeta = new JSONObject();
+            historyMeta.put("experience_gained", contigDifficulty.toString());
+            historyMeta.put("date", this.currDate());
+
+            //add meta object to user's annotation history and the id of the
+            //annotation
+            JSONObject historyJSON = new JSONObject();
+
+            historyJSON.put("meta", historyMeta);
+            historyJSON.put("anno_id", anno_id);
+
+            userHistory.put(historyJSON);
+
+            //re-add the history json to the user's document
+            userJSON.put("history", userHistory);
+
+            /*###############################################################*/
+            //USER INCOMPLETE ANNOTATIONS
+
+            /*
+             * Remove annotation from user's incomplete annotations if it is present
+             */
+            JSONArray inc_annotations = userJSON.getJSONArray("incomplete_annotations");
+
+            for (int ann_ndx = 0; ann_ndx < inc_annotations.length(); ann_ndx++) {
+               String inc_annotation = userHistory.getString(ann_ndx);
+
+               if (annotationIdList.get(iter_ndx).equals(inc_annotation)) {
+                  inc_annotations.remove(ann_ndx);
+                  break;
+               }
+            }
+            userJSON.put("incomplete_annotations", inc_annotations);
+
+            /*###############################################################*/
+            //ANNOTATION META
+
+            /*
+             * Modify annotation to be marked as finished.
+             */
+            JSONObject annotationMeta = annotationJSON.getJSONObject("meta");
+            annotationMeta.put("finished", this.currDate());
+            annotationMeta.put("last_modified", this.currDate());
+
+            annotationJSON.put("meta", annotationMeta);
+
+            /*###############################################################*/
+
+            /*
+             * Overwrite the old user and annotation documents with the updated
+             * user and annotation documents
+             */
+            client.set(user_id, 0, userJSON);
+            client.set(anno_id, 0, annotationJSON);
          }
          catch (Exception ex) {
             System.err.println("Error in PublishWorkload Couchbase code: " + ex);
             System.exit(1);
          }
 
-         //TODO: Still need to increase the experience gained in user meta
-         //table as well as add annotation to user history?
       }
 
       return new Stats();
-      */
-      throw new UnsupportedOperationException();
    }
 }
